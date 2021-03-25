65ad91b5cb56f91c91264911933352f064cf10ca
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-03-25 13:55:12 +0100

    adding snack 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,72 @@
 /**
  * 
+ *  SNACK 2
+ *  1. ASK TO INSERT A NUMBER
+ *  2. IF THE NUMBER IS EVEN THEN PRINT IT
+ *  3. IF THE NUMBER IS ODD THEN PRINT THE FOLLOWING ONE
+ * 
+/**/
+
+
+// VARIABLES
+
+var userNumber = parseInt( prompt('Please enter a Number here') );
+
+// EVENTS
+
+if(userNumber % 2 === 0) {
+    console.log(userNumber);
+} else {
+    console.log(userNumber + 1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
  *  SNACK 1
  *  1. ASK 5 TIMES TO INSERT A NUMBER
  *  2. PRINT THE SUM OF ALL THE NUMBERS
