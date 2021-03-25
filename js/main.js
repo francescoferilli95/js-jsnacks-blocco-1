bb5c6970881caeda8b2ee72be02a088c334552a3
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-03-25 13:51:24 +0100

    adding snack 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,53 @@
 /**
  * 
- * 
- * 
+ *  SNACK 1
+ *  1. ASK 5 TIMES TO INSERT A NUMBER
+ *  2. PRINT THE SUM OF ALL THE NUMBERS
+ *  3. DO THIS TASK WITH "FOR" && "WHILE"
  * 
 /**/
+
+// FOR
+
+// VARIABLES
+
+/*
+var userNumber;
+var requestedNumbers = 5;
+var sum = 0;
+
+// EVENTS
+
+for( var i = 1; i <= requestedNumbers; i++) {
+    userNumber = parseInt( prompt('Please enter a Number ' + i + ' of ' + requestedNumbers) );
+    console.log(userNumber);
+    sum += userNumber;
+}
+
+// OUTPUT
+
+console.log(sum);
+*/
+
+// WHILE
+
+/*
+// VARIABLES
+
+var userNumber;
+var requestedNumbers = 5;
+var counter = 1;
+var sum = 0;
+
+// EVENTS
+
+while( counter <= requestedNumbers) {
+    userNumber = parseInt( prompt('Please enter a Number ' + counter + ' of ' + requestedNumbers) );
+    console.log(userNumber);
+    sum += userNumber;
+
+    counter++;
+}
+
+console.log(sum);
+*/
